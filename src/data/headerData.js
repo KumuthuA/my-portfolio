bda426e46f5f98c749c483fd475fd8e2fe2571de
KumuthuA <kumuthua@gmail.com>
2024-03-16 05:23:18 +0530

    CV updated

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,4 @@
-import resume from '../assets/pdf/CV_Kumuthu_v4.pdf'
+import resume from '../assets/pdf/CV_Kumuthu_v5.pdf'
 import myimage from '../assets/png/me2.jpg'
 
 export const headerData = {
